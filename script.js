82e0bea3cb3df2064ac07d864e517fb7b4018c34
BananaGo69 (DanczMil) <dancza.milan@keativszolnok.hu>
2026-03-17 17:46:38 +0100

    JS rövidítés

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const image = document.getElementById("image");
 const NavUi = document.getElementById("NavUi");
-
-
 image.addEventListener("click", function(){
     image.classList.toggle("jelen");
     NavUi.classList.toggle("jelen");
